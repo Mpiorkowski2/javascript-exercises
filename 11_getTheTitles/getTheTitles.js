7b72072f2dc07ec29068526002dae244304831db
Mpiorkowski2 <piorkowskimartin83@gmail.com>
2024-09-14 21:24:27 -0500

    done with stupid ass exercises

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,6 +1,18 @@
-const getTheTitles = function() {
+const getTheTitles = function(fn){
+return fn.map((books) => books.title)
+}
 
-};
+
+const books = [
+    {
+      title: 'Book',
+      author: 'Name'
+    },
+    {
+      title: 'Book2',
+      author: 'Name2'
+    }
+  ]
 
 // Do not edit below this line
 module.exports = getTheTitles;
